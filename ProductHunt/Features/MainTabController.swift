3170424ee22017af99225aec2015e594863e2fd4
Ky Nguyen <nguyentruongky33@gmail.com>
2021-08-21 22:22:58 +0700

    Init project

---
+++ ProductHunt/Features/MainTabController.swift
@@ -0,0 +1,10 @@
+//  Created by Ky Nguyen
+
+import UIKit
+
+class MainTabController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
